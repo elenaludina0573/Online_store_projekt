9f5e11a4bdc0480d2dbef9fea9666643ad28ae59
elenaludina0573 <elenaludina0@gmail.com>
2024-04-08 19:15:43 +0300

    Filling_2 the db through the command

--- catalog/management/commands/fill_db.py
+++ catalog/management/commands/fill_db.py
@@ -1,66 +1,37 @@
-from django.core.management.base import BaseCommand
+import random
+
+from django.core.management import BaseCommand
+
 from catalog.models import Category, Product
-from django.conf import settings
-import json
-import os
 
 
 class Command(BaseCommand):
 
-    @staticmethod
-    def json_read_categories():
-        file_path = os.path.join(settings.BASE_DIR, 'fixtures', 'catalog_data.json')
-        with open(file_path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            category = []
-            for object in data:
-                if object.get('model') == 'catalog.category':
-                    category.append(object)
-            return category
-
-    @staticmethod
-    def json_read_products():
-        file_path = os.path.join(settings.BASE_DIR, 'fixtures', 'catalog_data.json')
-        with open(file_path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            category = []
-            for object in data:
-                if object.get('model') == 'catalog.product':
-                    category.append(object)
-            return category
-
     def handle(self, *args, **options):
-
-        Category.objects.all().delete()
-        Product.objects.all().delete()
-
-        # Создайте списки для хранения объектов
-        category_for_create = []
-        product_for_create = []
-
-        # Обходим все значения категорий из фиктсуры для получения информации об одном объекте
-        for category in Command.json_read_categories():
-            category_fields = category.get('fields')
-            category_for_create.append(
-                Category(id=category_fields.get('pk'), name=category_fields.get('name'),
-                         description=category_fields.get('description'))
+        category_list = []
+        for i in range(10):
+            category_list.append(
+                Category(
+                    name=f'Category#{i+1}',
+                    description=f'This is another category #{i+1}'
+                )
             )
 
-        # Создаем объекты в базе с помощью метода bulk_create()
-        Category.objects.bulk_create(category_for_create)
+        Category.objects.bulk_create(category_list)
 
-        # Обходим все значения продуктов из фиктсуры для получения информации об одном объекте
-        for product in Command.json_read_products():
-            product_fields = product.get('fields')
-            product_for_create.append(
-                Product(id=product_fields.get('pk'), name=product_fields.get('name'),
-                        description=product_fields.get('description'),
-                        # получаем категорию из базы данных для корректной связки объектов
-                        category=Category.objects.get(pk=product_fields.get('category')),
-                        purchase_price=product_fields.get('purchase_price'),
-                        created_at=product_fields.get('created_at'), updated_at=product_fields.get('updated_at')
-                        )
+        category_id_list = list(Category.objects.all().values_list('id', flat=True))
+        max_id = max(category_id_list)
+        min_id = min(category_id_list)
+
+        product_list = []
+        for i in range(100):
+            product_list.append(
+                Product(
+                    name=f'Product#{i}',
+                    description=f'This is description for product #{i}',
+                    category_id=random.randint(min_id, max_id),
+                    purchase_price=random.random() * 1000
+                )
             )
 
-        # Создаем объекты в базе с помощью метода bulk_create()
-        Product.objects.bulk_create(product_for_create)
+        Product.objects.bulk_create(product_list)
