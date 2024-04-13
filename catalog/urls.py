44ee722ddff23f2c57ef15940b698037d79d6299
elenaludina0573 <elenaludina0@gmail.com>
2024-04-13 18:21:48 +0300

    Addendum_2 a path

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,8 +5,8 @@ from catalog.apps import CatalogConfig
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('index/', index, name='index'),
-    path('', index_1, name='index_1'),
+    path('', index, name='index'),
+    path('index_1/<int:pk>', index_1, name='index_1'),
     path('contacts/', contact, name='contacts')
 ]
 
