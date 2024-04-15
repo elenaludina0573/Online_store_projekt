be2f847a550ab89c3f9e444820be2c6fbb3195d1
elenaludina0573 <elenaludina0@gmail.com>
2024-04-15 21:05:59 +0300

    Addendum_3 a path

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from catalog.views import index_1, contact, index
+from catalog.views import index, home, contacts
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('index_1/<int:pk>', index_1, name='index_1'),
-    path('contacts/', contact, name='contacts')
+    path('', home, name='home'),
+    path("contacts/", contacts, name="contacts"),
+    path('catalogs/<int:pk>', index, name='index'),
 ]
-
