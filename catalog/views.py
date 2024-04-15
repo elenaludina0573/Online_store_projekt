16fbc37e7ba409660185189f3b5de9197300d58a
elenaludina0573 <elenaludina0@gmail.com>
2024-04-15 21:08:30 +0300

    Addendum_2 a context

--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
-from catalog.models import Category
+from catalog.models import Product
 
 
-def index(request):
-    return render(request, 'catalog/home.html')
+def home(request):
+    products_list = Product.objects.order_by("updated_at")
+    context = {"products": products_list[:5:-1], "title": "Продукты на любой вкус"}
+    return render(request, "catalog/home.html", context)
 
 
-def index_1(request, pk):
+def index(request, pk):
+    product_item = Product.objects.get(pk=pk)
     context = {
-         'object_list': Category.objects.get(pk=pk),
-         'title': 'Магазин продуктов'
+        "product": Product.objects.filter(pk=pk),
+        "title": product_item.name,
     }
-    return render(request, 'catalog/index.html', context)
+    return render(request, "catalog/index.html", context=context)
 
 
-def contact(request):
+def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
