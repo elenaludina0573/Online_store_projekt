9ee59b2a13f146a4b2d0136bdaaf9a1ffa120e9e
elenaludina0573 <elenaludina0@gmail.com>
2024-04-23 12:44:52 +0300

    Changing

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from catalog.models import Product, Contacts, Blogpost
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from pytils.translit import slugify
 
 
 class ProductListView(ListView):
@@ -38,13 +38,18 @@ class BlogpostListView(ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Блог'
+        context['title'] = ' Блоговая запись'
         return context
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(publication_sign=True)
+        return queryset
 
 
 class BlogpostCreateView(CreateView):
     model = Blogpost
-    fields = '__all__'
+    fields = ['title', 'preview', 'publication_sign', 'number_of_views']
     template_name = 'catalog/blogpost_from.html'
     success_url = reverse_lazy('catalog:blogpost_form')
 
@@ -53,12 +58,34 @@ class BlogpostCreateView(CreateView):
         context['title'] = 'Добавить запись в блог'
         return context
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
 
 class BlogpostUpdateView(UpdateView):
     model = Blogpost
-    fields = '__all__'
+    fields = ['title', 'preview', 'publication_sign', 'number_of_views']
     template_name = 'catalog/blogpost_from.html'
-    success_url = reverse_lazy('catalog:blogpost_form')
+    #success_url = reverse_lazy('catalog:blogpost_form')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Изменить запись в блоге'
+        return context
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('catalog:blogpost_detail', kwargs={'pk': self.object.pk})
 
 
 class BlogpostDetailView(DetailView):
@@ -66,13 +93,26 @@ class BlogpostDetailView(DetailView):
     template_name = 'catalog/blogpost_detail.html'
     success_url = reverse_lazy('catalog:blogpost_detail')
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        blogpost_item = self.get_object()
+        context['blogpost_item'] = blogpost_item
+        context['title'] = f'Запись в блоге #{blogpost_item.id}'
+        return context
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.number_of_views += 1
+        self.object.save()
+        return self.object
+
 
 class BlogpostDeleteView(DeleteView):
     model = Blogpost
     template_name = 'catalog/blogpost_confirm_delete.html'
-    success_url = '/catalog/blogpost/'
+    success_url = reverse_lazy('catalog:blogpost_confirm_delete')
 
-
-
-
-
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Удалить запись из блога'
+        return context
