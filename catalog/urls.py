93fbf701812810117eae7308466e84852d0b5fdc
elenaludina0573 <elenaludina0@gmail.com>
2024-04-23 12:44:15 +0300

    Addendum_6 a path

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,7 +8,7 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
-    path('contacts/', ContactsView.as_view(), name="contacts"),
+    path('contacts/', ContactsView.as_view(), name="contacts_list"),
     path('catalog/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
     path('catalog/', BlogpostListView.as_view(), name='blogpost_list'),
     path('catalog/create/', BlogpostCreateView.as_view(), name='blogpost_form'),
