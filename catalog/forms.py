5fe3da21f370125c0189b03cea7c0f9293bf710c
elenaludina0573 <elenaludina0@gmail.com>
2024-05-05 20:20:22 +0300

    Creating  forms

---
+++ catalog/forms.py
@@ -0,0 +1,46 @@
+from django.db.models import BooleanField
+from django.forms import ModelForm, forms
+from catalog.models import Product
+
+
+forbidden_words = [
+                   'казино', 'криптовалюта', 'крипта', 'биржа',
+                   'дешево', 'бесплатно', 'обман', 'полиция', 'радар'
+]
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = Product
+        exclude = ['created_at', 'updated_at']
+
+    def clean_product_name(self):
+
+        clean_data = self.cleaned_data.get['name']
+        if clean_data in forbidden_words:
+            raise forms.ValidationError(f'Наименование не должно содержать слова: {forbidden_words}')
+        else:
+            return clean_data
+
+    def clean_product_description(self):
+        clean_data = self.cleaned_data.get['description']
+        if clean_data in forbidden_words:
+            raise forms.ValidationError(f'Наименование не должно содержать слова: {forbidden_words}')
+        else:
+            return clean_data
+
+
+class VersionForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = Product
+        exclude = ("is_active_version",)
