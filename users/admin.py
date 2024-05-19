7fbce3724c22a2f57b1655687781d4df7d514f12
elenaludina0573 <elenaludina0@gmail.com>
2024-05-19 23:10:55 +0300

    Addendum

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from users.models import User
 
-admin.site.register(User)
+#admin.site.register(User)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'email', 'is_active', 'is_staff', 'is_superuser']
+
+
