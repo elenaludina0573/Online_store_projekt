c2cc046ba241bf9ee894bb7d0bd2a286200a60c3
elenaludina0573 <elenaludina0@gmail.com>
2024-05-29 01:03:10 +0300

    Correction

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
             email='admin1@localhost',
-            first_name='Admin',
+            first_name='admin',
             password='Blev2011',
             is_superuser=True,
             is_staff=True,
